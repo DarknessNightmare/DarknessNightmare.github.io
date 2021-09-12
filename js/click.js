a1d8950557cdb9395b151d1028c5caaa39a34683
Darkness632 <81138097+Darkness632@users.noreply.github.com>
2021-09-12 12:07:10 +0700

    Add link via js

---
+++ js/click.js
@@ -0,0 +1,12 @@
+function openLink() {
+    window.open('https://api.whatsapp.com/send?phone=62857126693291&text=!help', '_blank')
+}
+function openIg() {
+    window.open('https://www.instagram.com/dxd.darkness', '_blank')
+}
+function openGroup() {
+    window.open('https://chat.whatsapp.com/DJEihZIBRg43ixkiHGwRQe', '_blank')
+}
+function openOwner() {
+    window.open('https://api.whatsapp.com/send?phone=628882856135&text=Halo owner DaBotz', '_blank')
+}
