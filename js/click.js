1344cb52b404b48b9c148e3272adb6b3c891a643
DarkNight <81138097+Darkness632@users.noreply.github.com>
2021-09-28 06:20:29 +0700

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,5 +1,5 @@
 function openLink() {
-    window.open('https://api.whatsapp.com/send?phone=62857126693291&text=!help', '_blank')
+    window.open('https://api.whatsapp.com/send?phone=380944318167&text=!help', '_blank')
 }
 function openIg() {
     window.open('https://www.instagram.com/dxd.darkness', '_blank')
@@ -8,7 +8,7 @@ function openGroup() {
     window.open('https://chat.whatsapp.com/DJEihZIBRg43ixkiHGwRQe', '_blank')
 }
 function openOwner() {
-    window.open('https://api.whatsapp.com/send?phone=628882856135&text=Halo owner DaBotz', '_blank')
+    window.open('https://api.whatsapp.com/send?phone=8616740842979&text=Halo owner DaBotz', '_blank')
 }
 function openEvent() {
     window.open('https://api.whatsapp.com/send?phone=62857126693291&text=!awgjkz', '_blank')
