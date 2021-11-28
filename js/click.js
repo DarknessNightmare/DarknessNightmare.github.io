fedc1a84aa342d1f16433439ce7531e7086a95bb
DarkNight <81138097+Darkness632@users.noreply.github.com>
2021-11-28 19:39:43 +0900

    Add files via upload

---
+++ js/click.js
@@ -0,0 +1,30 @@
+function openLink() {
+    window.open('https://api.whatsapp.com/send?phone=79532027785&text=!help', '_blank')
+}
+function openIg() {
+    window.open('https://www.instagram.com/dxd.darkness', '_blank')
+}
+function openGroup() {
+    window.open('https://chat.whatsapp.com/DJEihZIBRg43ixkiHGwRQe', '_blank')
+}
+function openOwner() {
+	window.open('https://api.whatsapp.com/send?phone=6285938633653&text=Halo owner Darkness-Bot', '_blank')
+}
+function openEvent() {
+	window.open('https://api.whatsapp.com/send?phone=79532027785&text=!event', '_blank')
+}
+function openBackup() {
+    window.open('https://api.whatsapp.com/send?phone=6285938633653&text=!help', '_blank')
+}
+function openGit() {
+    window.open('https://github.com/Darkness632', '_blank')
+}
+function openRep() {
+    window.open('https://api.whatsapp.com/send?phone=6285938633653&text=Bang Mau Lapor Ada Yang Eror', '_blank')
+}
+function openHal2() {
+    window.open('https://darkness632.github.io/index2.html', '_blank')
+}
+function openMap() {
+    window.open('https://darkness632.github.io', '_blank')
+}
