4dec05ebe2224ab4a493ff68139661bbae236a11
Darkness Nightmare <81138097+DarknessNightmare@users.noreply.github.com>
2022-02-10 08:48:18 +0700

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,5 +1,5 @@
 function openLink() {
-    window.open('https://api.whatsapp.com/send?phone=79532027785&text=!help', '_blank')
+    window.open('https://api.whatsapp.com/send?phone=6281211447153&text=!help', '_blank')
 }
 function openIg() {
     window.open('https://www.instagram.com/dxd.darkness', '_blank')
@@ -8,13 +8,13 @@ function openGroup() {
     window.open('https://chat.whatsapp.com/LTGxxBqjeK86ByvSEdafDR', '_blank')
 }
 function openOwner() {
-	window.open('https://api.whatsapp.com/send?phone=6285938633653&text=Halo owner Darkness-Bot', '_blank')
+	window.open('https://api.whatsapp.com/send?phone=84589488115&text=Halo owner Darkness-Bot', '_blank')
 }
 function openEvent() {
 	window.open('https://api.whatsapp.com/send?phone=79532027785&text=!event', '_blank')
 }
 function openBackup() {
-    window.open('https://api.whatsapp.com/send?phone=6285938633653&text=!help', '_blank')
+    window.open('https://api.whatsapp.com/send?phone=62857126693292&text=!help', '_blank')
 }
 function openGit() {
     window.open('https://github.com/Darkness632', '_blank')
